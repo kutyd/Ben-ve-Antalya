68cf0a39dc773ded3289877b78a1db9eae04f36c
kutyd <69771733+kutyd@users.noreply.github.com>
2021-05-11 00:34:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php echo "sayfaya hoşgeldiniz";?>
+<form action="bilgilerigoster.php" method="post">
+ <label for="adsoyad"> Ad Soyad:</label><input type="text" name="adsoyad" id="adsoyad"><br>
+ <label for="parola"> Parola:</label><input type="password" name="parola" id="parola"><br>
+ a
+<input type ="submit" value="Gönder">
+</form>
